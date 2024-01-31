be27656f49743ee4c7ddca41206257bf1ce836ca
Li Ma <lmaag@connect.ust.hk>
2024-01-31 03:42:35 +0000

    revise config

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ export HTTP_PROXY=http://58.34.83.134:31280/
 export HTTPS_PROXY=http://58.34.83.134:31280/
 export HF_HOME=/cpfs01/shared/pjlab-lingjun-landmarks/mali1/.cache
 
-accelerate launch marigold_train.py \
+accelerate launch --mixed_precision="fp16" marigold_train.py \
   --pretrained_model_name_or_path "stabilityai/stable-diffusion-2" \
   --val_data_dir examples \
   --output_dir "/cpfs01/shared/pjlab-lingjun-landmarks/mali1/outputs/try0" \
@@ -12,7 +12,7 @@ accelerate launch marigold_train.py \
   --train_batch_size 16 \
   --learning_rate 3e-05 \
   --max_train_steps 18000 \
-  --checkpointing_steps 2000 \
+  --checkpointing_steps 18000 \
   --gradient_accumulation_steps 2 \
   --lr_scheduler "cosine" --lr_warmup_steps 0 \
   --use_ema \
